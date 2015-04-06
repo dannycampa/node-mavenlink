5c1f8349790520eb920ee8dc8897e9a450977912
Fernando Herrera <fernandoh@lucidfusion.com>
2015-04-06 15:06:17 -0700

    Added PUT and DELETE commands

--- index.js
+++ index.js
@@ -121,7 +121,78 @@ function Maven(token, url, version){
             }); 
         }
             
-    }
+    };
+    
+    this.put = function post(endpoint, data, cb) {
+        
+        var make_request = true;
+        
+        if(endpoint == undefined || typeof endpoint != 'string') {
+            console.log('node-mavenlink Error: Your first Argument needs to be an endpoint as a string.');
+            make_request = false;
+        }
+        
+        if(data == undefined || typeof data != 'object') {
+            console.log('node-mavenlink Error: Please pass a second argument as an object of data.');
+            make_request = false;
+        }
+        
+        if(cb == undefined || typeof cb != 'function') {
+            console.log('node-mavenlink Error: Please pass a third argument as your callback function.');
+            make_request = false;
+        }
+        
+        if(make_request == false) {
+            console.log('node-mavenlink Error: You didn\'t properly pass the three arguments. No request made.');
+        } else {
+            var the_url = this.url_host + endpoint + '?' + param(data);
+            http.put({
+                url: the_url, 
+                headers: this.headers
+                }, function(error, response, body) {
+                    if(error) {
+                        return cb(error);
+                    } else {
+                        var the_body = JSON.parse(body);
+                        if(the_body.errors) {
+                            cb(the_body.errors);
+                    } else {
+                        var err = undefined;
+                        cb(err, the_body);
+                    }
+                }
+            }); 
+        }
+            
+    };
+    
+    this.delete = function post(endpoint, cb) {
+        
+        var make_request = true;
+        
+        if(endpoint == undefined || typeof endpoint != 'string') {
+            console.log('node-mavenlink Error: Your first Argument needs to be an endpoint as a string.');
+            make_request = false;
+        }
+                
+        if(cb == undefined || typeof cb != 'function') {
+            console.log('node-mavenlink Error: Please pass a third argument as your callback function.');
+            make_request = false;
+        }
+        
+        if(make_request == false) {
+            console.log('node-mavenlink Error: You didn\'t properly pass the three arguments. No request made.');
+        } else {
+            var the_url = this.url_host + endpoint;
+            http.del({
+                url: the_url, 
+                headers: this.headers
+                }, function() {
+                   return;
+            }); 
+        }
+            
+    };
     
 }
 
